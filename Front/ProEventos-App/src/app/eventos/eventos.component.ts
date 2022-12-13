07bc82c7729567371537f5b993804526ce290ce6
jacsonbleite <jacsonbleite@gmail.com>
2022-12-13 16:07:24 -0400

    compnents - nav e eventos- e recebendo e exibindo dados de API

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,30 @@ import { Component, } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent {
-  public eventos: any;
+  public eventos: any = [];
+  public filteredEvents: any = [];
+
+  widthImg: number = 70;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.filteredEvents = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+  }
+
+  filterEvents(filterBy: string): any{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -15,13 +38,22 @@ export class EventosComponent {
     this.getEventos();
   }
 
+  showOrHideImage() {
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void {
 
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.filteredEvents = this.eventos;
+      },
       error => console.log(error)
     );
 
   }
+
+
 
 }
